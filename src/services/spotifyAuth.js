63be14a06a28f4992e519341639c14a09ba8aa26
betel1221 <betelhem453@gmail.com>
2025-08-05 06:47:09 -0700

    fix: Correct Spotify auth variable assignment
    Fixes the incorrect assignment of clientId and edirectUri to their respective environment variables.

--- src/services/spotifyAuth.js
+++ src/services/spotifyAuth.js
@@ -1,6 +1,8 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
-const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
-const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+
+// Correct variable assignments
+const clientId = process.env.VITE_SPOTIFY_CLIENT_ID;
+const redirectUri = process.env.VITE_SPOTIFY_REDIRECT_URI;
 
 const scopes = [
   "user-read-private",
